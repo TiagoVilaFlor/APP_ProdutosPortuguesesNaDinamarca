c4447e4240a88f2c955e60bb040c44b4d50a86ac
Tiago Lourenço <tiago.vf.lourenco@gmail.com>
2026-01-28 16:27:08 +0100

    update

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export", // 🔥 STATIC EXPORT
-  images: {
-    unoptimized: true, // required for static export
-  },
-};
-
-module.exports = nextConfig;
